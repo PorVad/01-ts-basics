8183d2faa067347633fd1faa59a179416e1dfcea
PorVad <vadymysporadys@gmail.com>
2025-11-01 22:35:53 -0400

    J'en ai fini

---
+++ src/task-5.ts
@@ -0,0 +1,13 @@
+type LoadingStatus = "loading" | "success" | "error";
+
+function logStatus(status: LoadingStatus): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
